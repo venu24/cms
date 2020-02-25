8d0587ae976776dbd252749172d8128808cf89ef
venu24 <61458253+venu24@users.noreply.github.com>
2020-02-25 16:42:41 +0530

    Add files via upload

---
+++ CmsshoppingCart/src/main/java/com/cmsshopping/online/CmsshoppingCartApplication.java
@@ -0,0 +1,13 @@
+package com.cmsshopping.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsshoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsshoppingCartApplication.class, args);
+	}
+
+}
---
+++ CmsshoppingCart/src/test/java/com/cmsshopping/online/CmsshoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmsshopping.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsshoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
